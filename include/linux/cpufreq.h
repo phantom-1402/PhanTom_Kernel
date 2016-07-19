5ab41d9f56d04f5435ec88590def50d89aee8902
moresushant48 <moresushant48@gmail.com>
2016-07-19 13:57:49 +0000

    cpufreq: Darkness Support

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -435,6 +435,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_darkness)
 
 #endif
 
